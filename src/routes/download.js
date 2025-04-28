2682200505b99b8a10401170f39172b1407f8818
Nicolas <nicolas.gracioli@gmail.com>
2025-04-27 23:19:33 -0300

    feat: finished mp3 download process

--- src/routes/download.js
+++ src/routes/download.js
@@ -2,6 +2,42 @@ const express = require("express");
 const ytdl = require("@distube/ytdl-core");
 const router = express.Router();
 
-router.get("/download", async (req, res) => {});
+router.get("/download", async (req, res) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).send("URL is required");
+    }
+
+    try {
+        const info = await ytdl.getInfo(url);
+        const title = info.videoDetails.title
+            .replace(/[^a-z0-9]/gi, "_")
+            .toLowerCase();
+        const audioFormat = ytdl.chooseFormat(info.formats, {
+            quality: "highestaudio",
+            filter: "audioonly",
+        });
+
+        res.header(
+            "Content-Disposition",
+            `attachment; filename="${title}.mp3"`
+        );
+
+        ytdl.downloadFromInfo(info, {
+            format: audioFormat,
+            quality: "highestaudio",
+        })
+            .pipe(res)
+            .on("finish", () => {
+                console.log(`Downloaded: ${title}`);
+            });
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(500)
+            .send("An error occurred while processing your request");
+    }
+});
 
 module.exports = router;
