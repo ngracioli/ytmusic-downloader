b5af68ad08e9cca6cf05874474439925437f9e2e
Nicolas <nicolas.gracioli@gmail.com>
2025-04-27 23:19:03 -0300

    feat: create download route

---
+++ src/routes/download.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const ytdl = require("@distube/ytdl-core");
+const router = express.Router();
+
+router.get("/download", async (req, res) => {});
+
+module.exports = router;
