dfd24d6e778fd98b261d06eea7179912084d0f9d
Nicolas <nicolas.gracioli@gmail.com>
2025-04-27 23:18:12 -0300

    feat: create index.js with express

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <p>API IS RUNNING!</p>
+        <form action="/download" method="get">
+        <label for="url">YouTube URL:</label><br>
+        <input type="text" id="url" name="url" value=""><br>
+        <input type="submit" value="Download">
+        </form>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
